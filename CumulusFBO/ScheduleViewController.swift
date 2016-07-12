013479b71b0bc8ab306c19bf157721755a43be2e
Dan Patey <danpatey@danpatey.com>
2016-07-11 21:48:30 -0700

    Resolved UITable issue and removing redundant prepareForSegue

--- CumulusFBO/ScheduleViewController.swift
+++ CumulusFBO/ScheduleViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ScheduleViewController: UITableViewController {
-    var reservationStore: ReservationStore!
+    var reservationStore = ReservationStore()
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return reservationStore.allReservations.count
@@ -22,15 +22,5 @@ class ScheduleViewController: UITableViewController {
         cell.textLabel?.text = item.tailNumber
         
         return cell
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        let reservationStore = ReservationStore()
-        
-        let tabController = segue.destinationViewController as! UITabBarController
-        let reservationController = tabController.viewControllers![1] as! ScheduleViewController
-        
-        reservationController.reservationStore = reservationStore
-    }
-    
+    }    
 }
