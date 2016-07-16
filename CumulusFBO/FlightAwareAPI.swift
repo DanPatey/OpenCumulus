fd076a1e499de4a435c51b28dfeb4ba6fac2ec1f
Dan Patey <danpatey@danpatey.com>
2016-07-15 22:51:19 -0700

    Implemeting API helper file for flight tracking

---
+++ CumulusFBO/FlightAwareAPI.swift
@@ -0,0 +1,41 @@
+//
+//  FlightAwareAPI.swift
+//  CumulusFBO
+//
+//  Created by Dan Patey on 7/15/16.
+//  Copyright © 2016 Dan Patey. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+    case inFlightWeather = "InFlightInfo?ident=AAL1726"
+}
+
+struct FlightAwareAPI {
+    static let baseURLString = "https://danpatey:267d3e98c57a47c3b35dce1b17ce649da42ae225@flightxml.flightaware.com/json/FlightXML2"
+    private static let username = "danpatey"
+    private static let apiKey = "267d3e98c57a47c3b35dce1b17ce649da42ae225"
+
+    private static func flightAwareAPI(method method: Method, parameters: [String:String]?) -> NSURL {
+        let components = NSURLComponents(string: baseURLString)!
+        var queryItems = [NSURLQueryItem]()
+        
+        let baseParams = ["method": method.rawValue, "format": "json", "nojsoncallback": "1", "api_key": apiKey]
+        
+        for (key, value) in baseParams {
+            let item = NSURLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+        
+        if let additionalParams = parameters {
+            for (key, value) in additionalParams {
+                let item = NSURLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        components.queryItems = queryItems
+        
+        return components.URL!
+    }
+}
