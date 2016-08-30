7c0d3eceb0df434f56405b882feb13581d235207
Dan Patey <danpatey@danpatey.com>
2016-08-29 19:25:47 -0700

    Adding ChatViewController

---
+++ CumulusFBO/ChatViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ChatViewController.swift
+//  CumulusFBO
+//
+//  Created by Dan Patey on 8/29/16.
+//  Copyright © 2016 Dan Patey. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+}
--- CumulusFBO/MessagesLoginViewController.swift
+++ CumulusFBO/MessagesLoginViewController.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2016 Dan Patey. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import FirebaseAuth
+
+class MessagesLoginViewController: UIViewController {
+    
+    var ref: FIRAuth!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = FIRAuth.auth()
+    }
+    
+    @IBAction func loginDidTouch(sender: AnyObject) {
+        ref.signInAnonymouslyWithCompletion { (users, error) in
+            if error != nil { print(error!.description); return }
+        
+        print(self.ref.currentUser?.displayName)
+        self.performSegueWithIdentifier("LoginToChat", sender: self)
+        }
+    }
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        super.prepareForSegue(segue, sender: sender)
+//        let navigationViewController = segue.destinationViewController as! UINavigationController
+//        let chatViewController = navigationViewController.viewControllers.first as! ChatViewController
+//        chatViewController.senderId = "SomeUserId"
+//        chatViewController.senderDisplayName = "Some User"
+//    }
+}
