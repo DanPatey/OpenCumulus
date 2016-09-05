eb31062b23f209aa9ed416d38f1a156ec329f25c
fineprint <cnosborne15@yahoo.com>
2016-09-04 23:08:52 -0700

    Add contraints, background and buttons

---
+++ CumulusFBO/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  CumulusFBO
+//
+//  Created by Courtney Osborne on 9/4/16.
+//  Copyright © 2016 Dan Patey. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
